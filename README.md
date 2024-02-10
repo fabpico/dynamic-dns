2df4e61308263b0ead066dedef9a97f1cc6b2e65
Fabian Picone <fabian.picone@gmail.com>
2024-02-10 17:29:23 +0100

    Choose an app

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # Dynamic DNS
 
 Accessing a local network remotely with a domain, while having a dynamic IP address.  
-This method doesn't change DNS records on the domain, it works with a redirect.
+This method doesn't change DNS records on the domain, it works with IP caching.
 
-Example: foo.example.com?port=4533 -> 80.xx.xx.x:4533
-
-> **Why not using a service like noip.com?**
->
+> **Why not using a service like noip.com?**  
 > With a free membership, you'll receive a monthly email requiring multiple steps of confirmation to continue using the
 > service. After a while doing this, this process can become annoying.
 
 ## Requirements
 
 - Docker on a local machine
-- A domain that points to a webhosting (PHP/MySQL) 
+- A domain that points to a webhosting (PHP/MySQL)
 
 ## Setup webhosting
 
 1. Setup your domain to point to your webhosting (eg. foo.example.com)
-2. Create a database (for IP caching).
+2. Create a database with following table
    ```
    CREATE TABLE `ips` (
       `ip` VARCHAR(15) NOT NULL,
@@ -33,8 +30,3 @@ Example: foo.example.com?port=4533 -> 80.xx.xx.x:4533
 2. Build the docker container: Run `docker build --tag dynamic-dns .`
 3. Create a daily cron job for following command (this caches the dynamic IP)  
    `docker run -e DATABASE_HOST=xx -e DATABASE_USERNAME=xx -e DATABASE_PASSWORD=xx -e DATABASE_NAME=xx dynamic-dns`
-
-## Enjoy
-
-By calling your domain, it should redirect you to your local network.  
-You can optionally pass a port: foo.example.com?port=4533 -> 80.xx.xx.x:4533
--- webhosting/index.php
+++ webhosting/index.php
@@ -1,35 +1,20 @@
 <?php
-
 $databaseConfig = [
     'host' => 'xx',
     'username' => 'xx',
     'password' => 'xx',
     'database' => 'xx',
 ];
+$mysqli = new mysqli(
+    $databaseConfig['host'],
+    $databaseConfig['username'],
+    $databaseConfig['password'],
+    $databaseConfig['database'],
+);
+$result = $mysqli->query("SELECT ip, date_time FROM ips");
+$ipCache = $result->fetch_assoc();
 
-function getIpCache(array $databaseConfig): array
-{
-    $mysqli = new mysqli(
-        $databaseConfig['host'],
-        $databaseConfig['username'],
-        $databaseConfig['password'],
-        $databaseConfig['database'],
-    );
-    $result = $mysqli->query("SELECT ip, date_time FROM ips");
-    $row = $result->fetch_assoc();
-    return $row;
-}
-
-function createRedirectionUrl(string $ip): string
-{
-    $port = $_GET['port'] ?? '80';
-    return "http://$ip:{$port}";
-}
-
-$ipCache = getIpCache($databaseConfig);
-$redirectionUrl = createRedirectionUrl($ipCache['ip']);
 echo <<<HEREDOC
-
 <meta name="viewport" content="width=device-width,initial-scale=1"/>
 <style>
     body {
@@ -38,19 +23,15 @@ echo <<<HEREDOC
         color: white;
         text-align: center;
         padding-top: 100px;
-        line-height: 25px;
+    }
+    a {
+        color: white;
     }
 </style>
 
-<p>
-    Proceeding to {$ipCache['ip']}...<br/>
-    (last updated at {$ipCache['date_time']})
-</p>
+<p>Last IP update was at {$ipCache['date_time']}</p>
 
-<script>
-    setTimeout(function(){
-    window.location.href = '$redirectionUrl'
-    }, 4000)
-</script>
+<p><a target="_blank" href="http://{$ipCache['ip']}">My first app</a></p>
+<p><a target="_blank" href="http://{$ipCache['ip']}:4533">My second app (port 4533)</a></p>
 
 HEREDOC;
